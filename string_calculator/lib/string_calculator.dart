8b34697d9040d62405cddd9caf56ea499f0a2926
ayanshluminary-web <ayanshluminary@gmail.com>
2026-01-21 11:25:45 +0530

    supports new line as delimiter PASS

--- string_calculator/lib/string_calculator.dart
+++ string_calculator/lib/string_calculator.dart
@@ -3,7 +3,9 @@ class StringCalculator {
   int add(String numbers) {
   if (numbers.isEmpty) return 0;
 
-  final parts = numbers.split(',');
+  //final parts = numbers.split(',');
+  final parts = numbers.split(RegExp('[,\n]'));
+
   int sum = 0;
 
   for (var part in parts) {
